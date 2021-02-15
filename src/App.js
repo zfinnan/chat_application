31203f40ba37ff4ee3cc22bfce8b5cd5161c86c6
zfinnan <63361320+zfinnan@users.noreply.github.com>
2021-02-14 22:05:58 -0500

    add log in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm'
 import ChatFeed from './components/ChatFeed'
 
 import './App.css';
 
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine 
             height="100vh"
             projectID="a177d3ce-43c9-4947-8f29-1aa7ddcb562a"
-            userName="zfinnan"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
